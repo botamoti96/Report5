3f8c616e92fa51ac3b0061ff1d96f8e41b4e62ac
kazuki higa <e205759@ie.u-ryukyu.ac.jp>
2020-12-12 14:52:36 +0900

    6th commit

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        String str = "123";//"百二十三";
+        String str = "百二十三";
         int value = Integer.parseInt(str);
-        System.out.println(value);
     }
 }
